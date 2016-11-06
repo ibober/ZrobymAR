a6296b603790a70b7559d65524a9a2d081f109e3
Kiryl Ambrazheichyk <ibober182@gmail.com>
2016-11-06 09:23:58 +0300

    Последняя версия

---
+++ Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotate : MonoBehaviour {
+
+	void Update () {
+        transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
+    }
+}
