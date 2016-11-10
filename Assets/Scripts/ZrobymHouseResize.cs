905ede92f2d4fb9e7d56c70613a75c02fa3e2c9b
Kiryl Ambrazheichyk <ibober182@gmail.com>
2016-11-11 00:23:26 +0300

    Button experiments succeeded!
    Although it's working, it needs fixings to work properly.

--- Assets/Scripts/ZrobymHouseResize.cs
+++ Assets/Scripts/ZrobymHouseResize.cs
@@ -2,33 +2,39 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class ZrobymHouseResize : MonoBehaviour {
+public class ZrobymHouseResize : MonoBehaviour
+{
+    private bool isActive = true;
 
     public void ModuleAdd()
     {
         Debug.Log("! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! BUTTON CLICKED ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !");
+        GameObject.Find("Mass1").GetComponent<Renderer>().enabled =
+                !GameObject.Find("Mass1").GetComponent<Renderer>().enabled;
+
+
         //GameObject modularHouse = new GameObject();
-        if (GameObject.Find("longhouseAR") != null)
-        {
-            Debug.Log("Inside If");
-            //ARMarker[] markers = FindObjectsOfType(typeof(ARMarker)) as ARMarker[];
-            //foreach...
-            //if (myMarker.Visible) Debug.Log("Marker is visible.");
+        //if (GameObject.Find("longhouseAR") != null)
+        //{
+        //Debug.Log("Inside If");
+        //ARMarker[] markers = FindObjectsOfType(typeof(ARMarker)) as ARMarker[];
+        //foreach...
+        //if (myMarker.Visible) Debug.Log("Marker is visible.");
 
-            //GameObject.Find("longhouseAR").GetComponent<Renderer>().enabled =
-            //    !GameObject.Find("longhouseAR").GetComponent<Renderer>().enabled;
+        //GameObject.Find("longhouseAR").GetComponent<Renderer>().enabled =
+        //    !GameObject.Find("longhouseAR").GetComponent<Renderer>().enabled;
 
-            //if (GameObject.Find("longhouseAR").activeInHierarchy)
-            //{
-            //    GameObject.Find("longhouseAR").GetComponent()
-            //    Debug.Log("HIDING");
-            //}
-            //else
-            //{
-            //    GameObject.Find("longhouseAR").SetActive(true); //.enabled = false;
-            //    Debug.Log("SHOWING");
-            //}
-        }
+        //if (GameObject.Find("longhouseAR").activeInHierarchy)
+        //{
+        //    GameObject.Find("longhouseAR").GetComponent()
+        //    Debug.Log("HIDING");
+        //}
+        //else
+        //{
+        //    GameObject.Find("longhouseAR").SetActive(true); //.enabled = false;
+        //    Debug.Log("SHOWING");
+        //}
+        //}
     }
 
     public void ModuleRemove()
