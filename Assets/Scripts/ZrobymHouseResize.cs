ff225e8280578e56add6ebc313fad70ad9435794
Kiryl Ambrazheichyk <ibober182@gmail.com>
2016-11-12 23:29:19 +0300

    Nothing useful
    only gif logo components added

--- Assets/Scripts/ZrobymHouseResize.cs
+++ Assets/Scripts/ZrobymHouseResize.cs
@@ -4,37 +4,11 @@ using System.Collections;
 
 public class ZrobymHouseResize : MonoBehaviour
 {
-    private bool isActive = true;
-
     public void ModuleAdd()
     {
         Debug.Log("! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! BUTTON CLICKED ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !");
-        GameObject.Find("Mass1").GetComponent<Renderer>().enabled =
-                !GameObject.Find("Mass1").GetComponent<Renderer>().enabled;
-
-
-        //GameObject modularHouse = new GameObject();
-        //if (GameObject.Find("longhouseAR") != null)
-        //{
-        //Debug.Log("Inside If");
-        //ARMarker[] markers = FindObjectsOfType(typeof(ARMarker)) as ARMarker[];
-        //foreach...
-        //if (myMarker.Visible) Debug.Log("Marker is visible.");
-
-        //GameObject.Find("longhouseAR").GetComponent<Renderer>().enabled =
-        //    !GameObject.Find("longhouseAR").GetComponent<Renderer>().enabled;
-
-        //if (GameObject.Find("longhouseAR").activeInHierarchy)
-        //{
-        //    GameObject.Find("longhouseAR").GetComponent()
-        //    Debug.Log("HIDING");
-        //}
-        //else
-        //{
-        //    GameObject.Find("longhouseAR").SetActive(true); //.enabled = false;
-        //    Debug.Log("SHOWING");
-        //}
-        //}
+        GameObject.Find("Mass2").GetComponent<Renderer>().enabled =
+                !GameObject.Find("Mass2").GetComponent<Renderer>().enabled;
     }
 
     public void ModuleRemove()
