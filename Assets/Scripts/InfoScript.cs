05f1c0f8814bd661e5007a84217d4ee592678f76
Kiryl Ambrazheichyk <ibober182@gmail.com>
2016-11-18 23:02:04 +0300

    Got everything but...
    ...nothing works and looks as it should

---
+++ Assets/Scripts/InfoScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InfoScript : MonoBehaviour
+{
+    public Image InfoImage;
+
+	public void ShowInfoScreen ()
+	{
+        //why the hell it doen't work??
+        InfoImage.enabled = !InfoImage.enabled;
+    }
+}
---
+++ Assets/Scripts/SplashShowCompanyAndGoAR.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashShowCompanyAndGoAR : MonoBehaviour
+{
+    public float TimeBeforeName = 3.0f;
+    public float TimeBeforeAugmentation = 1.5f;
+    public Text CompanyName;
+
+    void Start ()
+    {
+        CompanyName.text = "";
+        Invoke("ShowCompanyName", TimeBeforeName);
+    }
+
+    void ShowCompanyName()
+    {
+        CompanyName.text = "by\nZROBYM Architects";
+        Invoke("SwitchToARScene", TimeBeforeAugmentation);
+    }
+
+    void SwitchToARScene()
+    {
+        //dont know why Unity crashes with it
+	    //SceneManager.LoadScene("AugmentedReality");
+    }
+}
