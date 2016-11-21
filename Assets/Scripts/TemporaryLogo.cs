59017bbd3af46484f9c21f372135db209083a383
Kiryl Ambrazheichyk <ibober182@gmail.com>
2016-11-22 00:23:43 +0300

    VS wont deploy
    And other interesting stopped working stuff

---
+++ Assets/Scripts/TemporaryLogo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TemporaryLogo : MonoBehaviour
+{
+    public Image TempLogo;
+    public Image MH;
+
+	void Start ()
+	{
+	    Invoke("Hide",1f);
+	}
+	
+	void Hide ()
+	{
+	    TempLogo.enabled = false;
+	    MH.enabled = false;
+	}
+}
