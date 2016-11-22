cf1534fb2896c1eaea1f9cf0c0914608af8a8d19
Marina <minskas.marina@gmail.com>
2016-11-22 22:24:52 +0300

    добавлена анимация при загрузке (запускать со сцены LoadScean)

---
+++ Assets/Scripts/LoadScean.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScean : MonoBehaviour {
+
+    public bool animationEnd;
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (animationEnd)
+        {
+            SceneManager.LoadScene("AugmentedReality");
+        }
+	}
+}
