b6a28738787dc1300918ae17daee20d13c4607fd
Marina <minskas.marina@gmail.com>
2016-11-23 12:03:57 +0300

    Построение модели по элементам

---
+++ Assets/Scripts/ModuleAnimation.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class ModuleAnimation : MonoBehaviour {
+
+    public Transform[] component;
+    int countComponent = 0;
+    bool next = true;
+
+    void Start()
+    {
+        countComponent = transform.childCount;
+        component = new Transform[countComponent];
+
+        for (int i = 0; i < countComponent; i++)
+        {
+            component[i] = transform.GetChild(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+        if (gameObject.activeSelf)
+        {
+            for (int i = 0; i < countComponent; i++)
+            {
+                component[i].gameObject.SetActive(false);
+            }
+                StartCoroutine(spawnElement());
+               
+            }
+
+        }
+
+    IEnumerator spawnElement()
+    {
+        for (int i = 0; i < countComponent; i++)
+        {
+            if (!component[i].gameObject.activeSelf)
+            {
+                yield return new WaitForSeconds(0.02f);
+                component[i].gameObject.SetActive(true);
+
+
+            }
+        }
+    }
+    }
+
+
+
+        
+	
+	
+
