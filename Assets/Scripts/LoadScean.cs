33d78c25d566c710f6aa81222137a2f5b0086e15
Kiryl Ambrazheichyk <ibober182@gmail.com>
2016-12-20 01:41:16 +0300

    Lights on!

--- Assets/Scripts/LoadScean.cs
+++ Assets/Scripts/LoadScean.cs
@@ -9,12 +9,16 @@ public class LoadScean : MonoBehaviour {
     public float SplashTime = 5.0f;
     public Image SplashPanel;
     public Image SplashLogo;
+    public GameObject ArControllerGameObject;
 
     void Update () {
 
         if (animationEnd)
         {
-            Invoke("FinishAnimatedLogo", SplashTime);
+            if (!SplashPanel.IsDestroyed())
+            {
+                Invoke("FinishAnimatedLogo", SplashTime);
+            }
             //StartCoroutine("FinishAnimatedLogo");
             //SceneManager.LoadSceneAsync("AugmentedReality");
         }
@@ -23,6 +27,7 @@ public class LoadScean : MonoBehaviour {
     /*IEnumerator*/void FinishAnimatedLogo()
     {
         //yield return null;
+        ArControllerGameObject.GetComponent<ARController>().StartAR();
         Destroy(SplashPanel);
         Destroy(SplashLogo);
     }
